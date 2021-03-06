c31764a728c8e6e02b193439f9d09ab93d547d1f
Maxx <maxhiga.hawaii@gmail.com>
2021-03-06 10:02:41 -1000

    added starter code for express server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const path = require("path");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+}
+
+app.listen(PORT, () => {
+  console.log(`Listening at http://localhost:${PORT}`);
+});
