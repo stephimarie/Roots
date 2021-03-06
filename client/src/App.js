68347a02d7168f08546d9a91659ddf8418d8ea12
Maxx <maxhiga.hawaii@gmail.com>
2021-03-06 10:10:10 -1000

    set up app for concurrently

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Through your roots we grow together.
-        </p>
+        <p>Through your roots we grow together.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-        </a>
+        ></a>
       </header>
     </div>
   );
