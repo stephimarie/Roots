d505924002076680e518064ba1879d9ad0c756e1
LZ <laraezegart@gmail.com>
2021-03-11 20:19:32 -0800

    updating slogan and title in nav

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -22,7 +22,7 @@ const Register = () => {
   return (
     <div>
       <h4>Through your roots we grow together.</h4>
-      <h6>The app that creates cross-cultural and multilingual conversations.</h6>
+      <h6>The app that allows you to connect cross-culturally and create multilingual conversations!</h6>
 
       <form onSubmit={submit} className="reg-form">
         <label>Email</label>
