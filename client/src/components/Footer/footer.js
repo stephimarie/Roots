344a14cea5d504d6484bc34f278d8efccdc982e6
Maxx <maxhiga.hawaii@gmail.com>
2021-03-13 09:56:51 -1000

    fixing stash change

--- client/src/components/Footer/footer.js
+++ client/src/components/Footer/footer.js
@@ -1,23 +1,12 @@
 import React from "react";
 import "./style.css";
 
-<<<<<<< Updated upstream
-function Footer () {
-    return (
-    
-        <div className="footer">
-            <h4 id="text">Copy</h4>
-            
-        </div>
-    )
-=======
 function Footer() {
   return (
     <div className="footer">
       <h4 id="text">Copy</h4>
     </div>
   );
->>>>>>> Stashed changes
 }
 
 export default Footer;
--- client/src/components/Navbar/nav.js
+++ client/src/components/Navbar/nav.js
@@ -2,34 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../../pages/Login";
 import "../../pages/Register";
-<<<<<<< Updated upstream
-import "./style.css"
-
-
-function Nav (){
-
-    return (
-        <nav>
-        <div className="nav-wrapper">
-          <h2 className="brand-logo">Roots</h2>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/register">Register</Link></li>
-
-          </ul>
-        </div>
-      </nav>
-            
-    )
-=======
 import "./style.css";
+
 function Nav() {
   return (
     <nav>
       <div className="nav-wrapper">
-        <a href="#" className="brand-logo">
-          Roots
-        </a>
+        <h2 className="brand-logo">Roots</h2>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li>
             <Link to="/login">Login</Link>
@@ -41,7 +20,6 @@ function Nav() {
       </div>
     </nav>
   );
->>>>>>> Stashed changes
 }
 
 export default Nav;
