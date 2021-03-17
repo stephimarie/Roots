edc48a7499c9630c1dc008362a79e0db3176dc12
Nhi Danis <a.nhi1001@gmail.com>
2021-03-16 20:40:42 -0700

    fixed border bottom color

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -24,17 +24,17 @@ const Register = () => {
       <form onSubmit={submit} className="reg-form">
         <h1 style={{paddingTop:"20px"}}>SignUp:</h1>
         <label style={{color:"black"}}>Email</label>
-        <input onChange={onChange} type="text" name="email" />
+        <input style={{color:"black", borderBottom: "1px solid grey" }} onChange={onChange} type="text" name="email" />
         <label style={{color:"black"}}>Password</label>
-        <input onChange={onChange} type="text" name="password" />
+        <input style={{color:"black", borderBottom: "1px solid grey" }} onChange={onChange} type="text" name="password" />
         <label style={{color:"black"}}>Password Check</label>
-        <input onChange={onChange} type="text" name="passwordCheck" />
+        <input style={{color:"black", borderBottom: "1px solid grey" }} onChange={onChange} type="text" name="passwordCheck" />
         <label style={{color:"black"}}>Display Name</label>
-        <input onChange={onChange} type="text" name="displayName" />
+        <input style={{color:"black", borderBottom: "1px solid grey" }} onChange={onChange} type="text" name="displayName" />
         <label style={{color:"black"}}>Native Language</label>
-        <input onChange={onChange} type="text" name="native_lang" />
+        <input style={{color:"black", borderBottom: "1px solid grey" }} onChange={onChange} type="text" name="native_lang" />
         <label style={{color:"black"}}>Language you want to learn:</label>
-        <input onChange={onChange} type="text" name="learn_lang" />
+        <input style={{color:"black", borderBottom: "1px solid grey" }} onChange={onChange} type="text" name="learn_lang" />
         <input style={{marginTop:"20px", backgroundColor:"lightGrey", borderRadius:"10px"}} type="submit" name="Register" />
       </form>
     </div>
