f12e8f192bef28dca69d8a53dd3dfa9a5609a9c6
Nhi Danis <a.nhi1001@gmail.com>
2021-03-20 11:56:10 -0700

    update footer

--- client/src/components/Footer/footer.js
+++ client/src/components/Footer/footer.js
@@ -4,7 +4,13 @@ import "./style.css";
 function Footer() {
   return (
     <div className="footer">
-      <h5 id="text">&copy;Copy</h5>
+       <p id="team">
+        <h4 ><strong >|| Roots || </strong> </h4>
+        <a href="https://github.com/stephimarie" target="blank"> || Stephanie McCandless</a> || <a href="https://github.com/NhiDanis"
+          target="blank">Nhi Danis</a> || <a href="https://github.com/maxx-808" target="blank">Max Higa</a> || <a
+          href="https://github.com/cadeburkett" target="blank">Michael Burkett</a> || <a href="https://github.com/lzegart"
+          target="blank">Lara Zegart</a> ||
+      </p>
     </div>
   );
 }
