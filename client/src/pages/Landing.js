9705a1dc4dc953f62ca2da185311397435dd4ee1
Nhi Danis <a.nhi1001@gmail.com>
2021-03-20 12:08:04 -0700

    add color

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -6,7 +6,7 @@ import {Link} from "react-router-dom"
 const Landing = () => {
     return (
         <div style={{marginTop:"40px"}}>
-            <h5 style={{marginBottom: "40px", fontFamily:"'Akaya Telivigala', cursive"}}><strong>Sign-up for Roots Today!</strong></h5>
+            <h5 style={{marginBottom: "40px", fontFamily:"'Akaya Telivigala', cursive", color:"black"}}><strong>Sign-up for Roots Today!</strong></h5>
         <Auth>
             <Link className="log-Reg" to="/login"><strong>Login</strong></Link>
             <Link className="log-Reg" to="/register"><strong>Register</strong></Link>
