11bd9fc46b8b5922252a3a028a8dfa21fb4e2eef
Maxx <maxhiga.hawaii@gmail.com>
2021-03-24 17:58:56 -1000

    finsihed backend work for profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -49,7 +49,32 @@ const Profile = () => {
         <li>{profile.native_lang}</li>
         <li>{profile.learn_lang}</li>
       </ul>
-      <div className="post"></div>
+      <div
+        style={{ marginBottom: "70px" }}
+        className="container"
+        id="profilePost"
+      >
+        <h1>Personal Post</h1>
+        <div
+          style={{
+            background: "rgba(255, 255, 255, 0.250)",
+            marginTop: "50px",
+            borderRadius: "15px",
+            padding: "15px",
+          }}
+          className="card"
+        >
+          <p>{selfPost.message}</p>
+          {selfPost.chat.map((msg, index) => (
+            <div key={index}>
+              <span>
+                <strong>{msg.displayName}: </strong>
+              </span>
+              <span>{msg.chat}</span>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
